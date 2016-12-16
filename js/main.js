a54af5a4dca8a3f1c5efaa5742dbce44f6d038f3
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-16 14:56:16 +0200

    Add functionality

--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
 /*
  * Created by Sestri4kina on 15.12.2016.
  */
+var clickCounter = 1;
 var infoContainer = document.getElementById('info');
 var btn = document.getElementById('btn');
 
 btn.addEventListener('click', function(){
     var myRequest = new XMLHttpRequest();
-    myRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json', true);
+    myRequest.open('GET', 'js/part-'+ clickCounter +'.json', true);
     myRequest.onload = function(){
         var myData = JSON.parse(myRequest.responseText);
         //console.log(myData[0]);
         renderHTML(myData);
     };
     myRequest.send();
+    clickCounter++;
+    if (clickCounter > 3){
+        btn.classList.add('hide-me');
+    }
 });
 
-function myData(data){
+function renderHTML(data){
+    var htmlString = '';
+    for(i = 0; i < data.length; i++){
 
+        if (Array.isArray(data[i].properties)){
+            var stringProp = data[i].properties.join(', ');
+        } else {
+            var stringProp = data[i].properties;
+        }
+
+        htmlString += '<p>' + data[i].name + ': ' + stringProp + '.</p>';
+    }
+    infoContainer.insertAdjacentHTML('beforeend', htmlString);
 }
