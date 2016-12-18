dff18f71c783b4805fac7f6b8327245f87b9f8d4
Anastasiia Bratochkina <abratochkina@gmail.com>
2016-12-18 20:50:18 +0200

    Learn ready states in async.js

---
+++ js/async.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Sestri4kina on 18.12.2016.
+ */
+window.onload = function(){
+
+    var http = new XMLHttpRequest();
+
+    http.onreadystatechange = function(){
+        if (http.readyState == 4 && http.status == 200){
+            console.log(JSON.parse(http.response));
+        }
+    };
+
+    http.open('GET', 'js/part-1.json', true);
+    http.send();
+    console.log('test');
+
+};
+
+/* READY STATES
+
+0 - request not initialized
+1 - request has been set up
+2 - request has been sent
+3 - request is in process
+4 - request is complete
+
+
+ */
